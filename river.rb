5220e98f667efea0a3e4f88724aac4b818af2cff
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-12 19:36:15 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,52 @@
 # river.rb
+class River 
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def countries=(countries)
+    @countries = countries
+  end 
+  
+  def countries
+    @countries 
+  end 
+  
+    def discharge=(discharge)
+    @discharge = discharge
+  end 
+  
+  def discharge
+    @discharge 
+  end 
+  
+  def length=(length)
+    @length = length
+  end 
+  
+  def length
+    @length 
+  end 
+  
+  def flood 
+    @discharge=(@discharge*1.3)
+  end 
+  
+  def dry_up 
+    @discharge = (@discharge*0.5)
+  end 
+  
+end 
+
+mississippi = River.new("Mississippi")
+mississippi.discharge=(593)
+puts mississippi.flood
